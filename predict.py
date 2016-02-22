384137a4eff01fb8099a87cc73a24fb70f763ee4
MasazI <masaz.dream@gmail.com>
2016-02-22 19:20:57 +0900

    movie prediction.

--- predict.py
+++ predict.py
@@ -15,10 +15,11 @@ import pandas as pd
 from PIL import Image as PILImage
 import cStringIO as StringIO
 import caffe
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-image_size = 500
+import cv2
 
+image_size = 100
 
 pallete = [0,0,0,
              128,0,0,
@@ -57,12 +58,41 @@ class CRFasRNN:
     def __init__(self, model_file, trained_model):
         self.model_file = model_file
         self.trained_model = trained_model
-        
         self.net = caffe.Segmenter(model_file, trained_model, gpu=True)
 
-    def predict(self, image_file_path):
-        input_image = 255 * caffe.io.load_image(image_file_path)
+    def predict_image(self, mat, debug=True):
+        '''
+        arguments:
+            image: opencv format image.
+        return:
+            labeld image
+            ratio
+        '''
+        if debug:
+            print mat.shape
+            print mat
 
+        input_image = mat[:,:,(2,1,0)]
+        input_image = input_image.astype(np.float)
+        if debug:
+            print input_image.shape
+            print input_image
+
+        original_width = input_image.shape[1]
+        original_height = input_image.shape[0]
+        ratio = .0
+        if original_width > original_height:
+            print("change scale with width")
+            ratio = 200./original_width
+        else:
+            print("change scale with height")
+            ratio = 200./original_height
+
+        print("width: %f --> %f" % (original_width, original_width * ratio))
+        print("height: %f --> %f" % (original_height, original_height * ratio))
+
+        input_image = caffe.io.resize_image(input_image, (int(original_height * ratio), int(original_width * ratio)))
+        
         width = input_image.shape[0]
         height = input_image.shape[1]
         maxDim = max(width, height)
@@ -78,11 +108,12 @@ class CRFasRNN:
 
         cur_h, cur_w, cur_c = im.shape
 
-        if cur_h >= 500 or cur_w >= 500:
+        if cur_h >= 250 or cur_w >= 250:
+            print("image is too big.")
             return
 
-        pad_h = 500 - cur_h
-        pad_w = 500 - cur_w
+        pad_h = 250 - cur_h
+        pad_w = 250 - cur_w
 
         im = np.pad(im, pad_width=((0, pad_h), (0, pad_w), (0, 0)), mode='constant', constant_values=0)
 
@@ -92,12 +123,33 @@ class CRFasRNN:
         output_im = PILImage.fromarray(segmentation2)
         output_im.putpalette(pallete)
 
-        plt.imshow(output_im)
-        plt.savefig('output.png')
+        mat_predicted = np.asarray(output_im)
+
+        print mat_predicted.shape
+        print mat_predicted
+
+        if debug:
+            #cv2.imwrite('output_cv_im.png', mat_predicted*255)
+            cv2.imshow('labeled each pixcels.', mat_predicted*255)
+            cv2.waitKey(0)
+        return mat_predicted, ratio
+
+
+    def predict(self, image_file_path):
+        mat = cv2.imread(image_file_path)
+        predicted, _ = self.predict_image(mat)
+        
 
 if __name__ == '__main__':
-        crf_as_rnn = CRFasRNN('TVG_CRFRNN_COCO_VOC.prototxt', 'models/train_iter_29000.caffemodel')
-        crf_as_rnn.predict('test_images/car.jpg')
+        argvs = sys.argv
+        argc = len(argvs)
+        if argc != 4:
+            print("Usage: predict.py <deploy prototxt> <trained model> <image path>")
+            exit()
 
+        deploy_prototxt = argvs[1]
+        trained_model = argvs[2]
+        image_path = argvs[3]
 
-
+        crf_as_rnn = CRFasRNN(deploy_prototxt, trained_model)
+        crf_as_rnn.predict(image_path)
---
+++ predict_movie.py
@@ -0,0 +1,77 @@
+import argparse
+
+import os
+import sys
+import os.path as osp
+
+
+import numpy as np
+from PIL import Image
+
+import chainer
+from chainer import cuda
+import chainer.functions as F
+from chainer.functions import caffe
+
+import time
+
+caffe_path = osp.join(os.getenv('CRF_AS_RNN_PATH'), 'caffe-crfrnn', 'python')
+print("add path: %s" % (caffe_path))
+sys.path.insert(0, caffe_path)
+
+from predict import CRFasRNN
+
+import cv2
+
+import cPickle
+import logging
+import numpy as np
+import pandas as pd
+
+parser = argparse.ArgumentParser(
+    description='Evaluate a Caffe reference model on ILSVRC2012 dataset')
+parser.add_argument('movie', help='Path to movie file')
+parser.add_argument('--skip', '-s', type=int, default=0, help='The number of skip frames.')
+args = parser.parse_args()
+
+
+# crf as rnn object
+crfasrnn = CRFasRNN('predict.prototxt', 'TVG_CRFRNN_COCO_VOC.caffemodel')
+
+# get video capture
+cap = cv2.VideoCapture(args.movie)
+cnt = args.skip
+
+print cap.isOpened()
+
+#cv2.imshow('predicted Each Pixcel Demo.')
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    height = frame.shape[0]
+    width = frame.shape[1]
+
+    if frame is None:
+        cnt = cnt + 1
+        cap.set(1, cnt)
+        print 'stop'
+        exit()
+
+    mat_predicted, ratio = crfasrnn.predict_image(frame, debug=False)
+
+
+    mat_predicted_orgsize = cv2.resize(mat_predicted*255, (width, height))
+    cv2.imshow("Original Movie.", frame)
+
+    cv2.imshow('Predicted Each Pixcel Demo.', mat_predicted_orgsize)
+    cv2.waitKey(20)
+
+    cnt = cnt + 5
+    # CV_CAP_PROP_POS_FRAMES is 1   
+    cap.set(1, cnt)
+
+
+cap.release()
+cv2.destroyAllWindows()
+
